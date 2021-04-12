115e3402305391f016c6e970f5e8b67bf92c1312
ARAA <afsalahamed07@gmail.com>
2021-04-12 15:21:35 +0530

    complete cli version

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # rock-paper-scissor
+
+## condition
+
+rock beats scissor (0 beats 2)
+paper beats rock (1 beats 0)
+scissor beats paper (2 beats 1)
+
+### conditon combination
+
+**[playerSelection, computerSelection]**
+["rock", "rock"]
+["rock", ""paper"]
+["rock", "scissor"]
+
+["papper", "rock"]
+["papper", "paper"]
+["papper", "scissor"]
+
+["scissor", "rock"]
+["scissor", "paper"]
+["scissor", "scissor"]
+
+get rid of same choice
+
+## Plan
+
+get number between (0-2) each repersent a weapon (use list of weapons)
+["rock", "paper", "scissor"]
+
+get user input as **playerSelction**
+get computer choice as **computerSelcction**
+for computer selection (**computerSelection**) use fun (**computerPlay**) to return random value
+```Math.floor((Math.random() * (3-0)))```
+
+create fun (**playRound**) to verify the conditons
+this returns winner
+
+### playRound
+
+use **conditon combiantion**
+
+## algorithm
---
+++ external.js
@@ -0,0 +1,50 @@
+const weapons = ["rock", "paper", "scissor"];
+const conCombination = [
+    ["rock", "scissor"],
+    ["paper", "rock"],
+    ["scissor", "paper"]
+];
+
+game();
+
+function computerPlay(weapons_array = weapons) {
+    return weapons_array[Math.floor((Math.random() * (3 - 0)))];
+}
+
+function playRound(computerSelection, playerSelection, conCombination_array = conCombination) {
+    let winner = "computer";
+
+    if (computerSelection === playerSelection) {
+        winner = "draw";
+    }
+
+    for (let i = 0; i < conCombination_array.length; i++) {
+        if (playerSelection === conCombination_array[i][0] && computerSelection === conCombination_array[i][1]) {
+            winner = "player";
+            break;
+        }
+
+    }
+
+    return winner;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Enter choice of weapon", "Rock")
+            .toLowerCase();
+
+        let winner = playRound(computerSelection, playerSelection);
+
+        if (winner === "player") {
+            console.log(`You win; ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats
+                 ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`);
+        } else if (winner === "draw") {
+            console.log("Draw")
+        } else {
+            console.log(`You lose; ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats
+            ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
+        }
+    }
+}
